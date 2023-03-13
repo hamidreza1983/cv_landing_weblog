0ea409f57aba67ce39d5f46ce08ee1fe48a17ffa
hamidreza1983 <126154017+hamidreza1983@users.noreply.github.com>
2023-03-13 18:07:35 +0330

    create new template
    all things changed

--- my_weblog/models.py
+++ my_weblog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
+class post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
 # Create your models here.
--- my_weblog/views.py
+++ my_weblog/views.py
@@ -3,4 +3,17 @@ from django.shortcuts import render
 def home(req):
     return render(req,'my_weblog/index.html')
 
-# Create your views here.
+def about(req):
+    return render(req,'my_weblog/about.html')
+
+def contact(req):
+    return render(req,'my_weblog/contact.html')
+
+def b_home(req):
+    return render(req,'my_weblog/blog-home.html')
+
+def b_single(req):
+    return render(req,'my_weblog/blog-single.html')
+
+
+
